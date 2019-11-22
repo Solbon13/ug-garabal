2863567a864cc5a0e911c3b8dc25fb04d2d2ee67
Solbon13 <Solbon13@yandex.ru>
2019-11-22 23:15:46 +0800

    test messages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import MessageList from "./components/MessageList";
+import Header from "./components/Header";
+import firebase from 'firebase';
+import MessageBox from "./components/MessageBox";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor(props){
+        super(props);
+        var сonfig = {
+            apiKey: "AIzaSyCl2bpoGi4VPXC8GfUQx7iz3rNHPSnQpDQ",
+            authDomain: "ug-garabal.firebaseapp.com",
+            databaseURL: "https://ug-garabal.firebaseio.com",
+            projectId: "ug-garabal",
+            storageBucket: "ug-garabal.appspot.com",
+            messagingSenderId: "692578517035",
+            appId: "1:692578517035:web:422b134779d9383f67ea6e",
+            measurementId: "G-Y7PJXXTRC4"
+        };
+        // Initialize Firebase
+        firebase.initializeApp(сonfig);
+    }
+
+
+  render() {
+    return (
+        <div className="container">
+            <Header title="Simple Firebase App" />
+            <div className="columns">
+                <div className="column is-3"></div>
+                <div className="column is-6">
+                    <MessageList db={firebase} />
+                </div>
+            </div>
+            <div className="columns">
+                <div className="column is-3"></div>
+                <div className="column is-6">
+                    {/*<MessageBox db={firebase} />*/}
+                </div>
+            </div>
+        </div>
+    );
+  }
 }
 
 export default App;
